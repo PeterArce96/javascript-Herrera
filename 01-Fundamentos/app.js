df5d314abf6f98f108171a2bab6ce6a3eb396e6c
PeterArce96 <parce0496@gmail.com>
2023-05-22 17:44:16 -0500

    javascript - Introducción a variables y comentarios

--- 01-Fundamentos/app.js
+++ 01-Fundamentos/app.js
@@ -1,1 +1,13 @@
-console.log("Hola Mundo");
+// console.log("Hola Mundo");
+
+//var --> forma antigua de crear variables
+
+// Polyfill --> código que provee el funcionamiento de una  nueva característica de Javascript(ES6), en versiones viejas como ES5 (funcion flecha, clases, etc), código que permite utilizar características modernas en versiones vieja de JS.
+
+let a = 10, 
+    b = 20, 
+    c = 10, 
+    d = 10, 
+    x = a + b;
+
+console.log(x);
