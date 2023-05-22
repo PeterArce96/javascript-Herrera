b058c7b535af4a648161aaeb4a47130696def03b
PeterArce96 <parce0496@gmail.com>
2023-05-22 18:42:15 -0500

    javascript - Order y lugar de las importaciones

---
+++ 01-Fundamentos/assets/js/app.js
@@ -0,0 +1,25 @@
+// console.log("Hola Mundo");
+
+let a = 10, 
+    b = 20, 
+    c = 30, 
+    d = 40, 
+    x = a + b;
+
+// console.log('%c Mis variables', 'color:blue; font-weight:bold'); //CSS
+// console.log( {a} );
+// console.log( {b} );
+// console.log( {c} );
+
+let e = 'Hola ';
+let f = 'Spiderman';
+
+// console.table({a,b,c,d,x,e,f});
+
+const saludo = e + f;
+
+// console.log(saludo)
+
+c = 'Hola de nuevo';
+
+var miNombre = "Fernando";
