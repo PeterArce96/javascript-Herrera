5c87048fadb238df83ee0dfe64954ec3501a8949
PeterArce96 <parce0496@gmail.com>
2023-05-22 18:00:43 -0500

    javascript - Introducción a la consola

--- 01-Fundamentos/app.js
+++ 01-Fundamentos/app.js
@@ -6,8 +6,25 @@
 
 let a = 10, 
     b = 20, 
-    c = 10, 
-    d = 10, 
+    c = 30, 
+    d = 40, 
     x = a + b;
 
-console.log(x);
+console.log('%c Mis variables', 'color:blue; font-weight:bold'); //CSS
+console.log( {a} );
+console.log( {b} );
+console.log( {c} );
+
+let e = 'Hola ';
+let f = 'Spiderman';
+
+console.table({a,b,c,d,x,e,f});
+
+const saludo = e + f; //concatena
+
+console.log(saludo)
+
+// el console es el objeto y log() es el metodo
+// Si colocamos el argumento entre llaves, nos muestra un objeto con su valor
+
+// JS es un lenguaje interpretado, porque el navegador va ejecutar las líneas en forma secuencial.
